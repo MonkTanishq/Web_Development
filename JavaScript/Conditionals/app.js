24414c39b99a4f53f2544e6f922fa97fdf169ec1
TANISHQ CHAUHAN <121804467+MonkTanishq@users.noreply.github.com>
2023-07-13 13:46:47 +0530

    Commit for Conditionals in JavaScript

---
+++ JavaScript/Conditionals/app.js
@@ -0,0 +1,35 @@
+// console.log("Before Conditionals")
+// let random = Math.random()
+// if(1 + 1 === 2)
+// {
+//     console.log("Math Still Works")
+// }
+// console.log("After Conditionals")
+
+// const dayOfWeek = 'Monday';
+
+// if(dayOfWeek === 'Monday')
+// {
+//     console.log("I hate mondays");
+// }
+// else if(dayOfWeek === 'Saturday')
+// {
+//     console.log("I like Saturday");
+// }
+
+const age = 80;
+if(age < 5)
+{
+    console.log("You are a baby. you get it for Free.")
+}
+else if(age < 10)
+{
+    console.log("You are a child. you pay 10$.")
+}
+else if(age < 65)
+{
+    console.log("You are a adult. you pay 20$.")
+}
+else{
+    console.log("You are a senior. you pay 10$.")
+}
